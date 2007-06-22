8fbd8100021c7120e572cd8cfc8f334b89ead2c4
carlossg <carlossg@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2007-06-22 23:20:02 +0000

    Move launch configuration to jdt ui

--- plugins/maven/jdt/ui/src/main/java/org/devzuz/q/maven/jdt/ui/launchconfiguration/MavenLaunchConfigurationDelegate.java
+++ plugins/maven/jdt/ui/src/main/java/org/devzuz/q/maven/jdt/ui/launchconfiguration/MavenLaunchConfigurationDelegate.java
@@ -4,7 +4,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  **************************************************************************************************/
-package org.devzuz.q.maven.ui.launchconfiguration;
+package org.devzuz.q.maven.jdt.ui.launchconfiguration;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
