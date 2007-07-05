3d549d14652a5622bae600e9e2b093ad23757452
emantos <emantos@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2007-07-05 09:42:07 +0000

    Added the 'Convert to Maven 2' menu item for ant and maven 1 projects. Added Maven2 icon on the Project creation wizard.

---
+++ plugins/maven/ui/src/main/java/org/devzuz/q/maven/ui/actions/MavenConvertAntAction.java
@@ -0,0 +1,25 @@
+package org.devzuz.q.maven.ui.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class MavenConvertAntAction implements IObjectActionDelegate
+{
+    public void setActivePart( IAction action, IWorkbenchPart targetPart )
+    {
+        
+    }
+
+    public void run( IAction action )
+    {
+        
+    }
+
+    public void selectionChanged( IAction action, ISelection selection )
+    {
+        
+    }
+
+}
---
+++ plugins/maven/ui/src/main/java/org/devzuz/q/maven/ui/actions/MavenConvertMaven1Action.java
@@ -0,0 +1,24 @@
+package org.devzuz.q.maven.ui.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class MavenConvertMaven1Action implements IObjectActionDelegate
+{
+    public void setActivePart( IAction action, IWorkbenchPart targetPart )
+    {
+        
+    }
+
+    public void run( IAction action )
+    {
+        
+    }
+
+    public void selectionChanged( IAction action, ISelection selection )
+    {
+        
+    }
+}
