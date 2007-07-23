282dcfd6e6769d168fa5e043bb50383a7c2eb0ca
xgenesis007 <xgenesis007@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2007-07-23 07:54:32 +0000

    Added EPL license header.

--- plugins/maven/wizard/src/main/java/org/devzuz/q/maven/wizard/importwizard/Maven2ImportWizard.java
+++ plugins/maven/wizard/src/main/java/org/devzuz/q/maven/wizard/importwizard/Maven2ImportWizard.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2007 DevZuz, Inc. (AKA Simula Labs, Inc.)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
 package org.devzuz.q.maven.wizard.importwizard;
 
 import org.devzuz.q.maven.jdt.ui.projectimport.ImportProjectJob;
