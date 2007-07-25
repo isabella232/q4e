b369e0f75c0e0249ed834bab779f553ed64a2c00
emantos <emantos@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2007-07-25 05:52:29 +0000

    Cleaned up the archetype table editor code.

--- plugins/maven/ui/src/main/java/org/devzuz/q/maven/ui/preferences/MavenArchetypePreferencePage.java
+++ plugins/maven/ui/src/main/java/org/devzuz/q/maven/ui/preferences/MavenArchetypePreferencePage.java
@@ -9,33 +9,28 @@ package org.devzuz.q.maven.ui.preferences;
 
 import org.devzuz.q.maven.embedder.Activator;
 import org.devzuz.q.maven.ui.Messages;
-import org.devzuz.q.maven.ui.preferences.editor.MavenArchetypePreferenceEditor;
+import org.devzuz.q.maven.ui.preferences.editor.MavenArchetypePreferenceTableEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
-
-public class MavenArchetypePreferencePage 
-	extends FieldEditorPreferencePage
-	implements IWorkbenchPreferencePage
-{	
-	public MavenArchetypePreferencePage()
-	{
-		super( GRID );
+public class MavenArchetypePreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage
+{
+    public MavenArchetypePreferencePage()
+    {
+        super( GRID );
         setPreferenceStore( Activator.getDefault().getPreferenceStore() );
-	}
-	
-	protected void createFieldEditors() 
-	{
-		noDefaultAndApplyButton();
-		addField( new MavenArchetypePreferenceEditor( Activator.PLUGIN_ID + Messages.MavenArchetypePreferenceEditor_name,
-				  Messages.MavenArchetypePreferencePage_description, getFieldEditorParent() ) ); 	
-	}
+    }
 
-	public void init(IWorkbench workbench) {
-		// TODO Auto-generated method stub
-		
-	}
+    protected void createFieldEditors()
+    {
+        noDefaultAndApplyButton();
+        addField( new MavenArchetypePreferenceTableEditor( Activator.PLUGIN_ID + Messages.MavenArchetypePreferenceEditor_name,
+                                                           Messages.MavenArchetypePreferencePage_description,
+                                                           getFieldEditorParent() ) );
+    }
 
-
+    public void init( IWorkbench workbench )
+    {
+    }
 }
