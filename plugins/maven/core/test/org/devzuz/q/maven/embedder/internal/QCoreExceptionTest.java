7ebd35b1fe925ce46e258f04b70c1dc6e8b322fd
carlossg <carlossg@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2007-08-28 11:21:42 +0000

    Add an exception that will expose the nested exception from CoreException' Status in the standard JDK 1.4 API so logging through log4j will print the right stack trace

---
+++ plugins/maven/core/test/org/devzuz/q/maven/embedder/internal/QCoreExceptionTest.java
@@ -0,0 +1,34 @@
+package org.devzuz.q.maven.embedder.internal;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.devzuz.q.maven.embedder.Activator;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+
+import junit.framework.TestCase;
+
+public class QCoreExceptionTest extends TestCase
+{
+    private static final String MSG = "nested exception";
+
+    private Exception coreException;
+
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        Exception e = new RuntimeException( MSG );
+        coreException =
+            new RuntimeException( new QCoreException( new Status( IStatus.ERROR, Activator.PLUGIN_ID,
+                                                                 "Unable to read project", e ) ) );
+    }
+
+    public void testPrintStackTrace()
+    {
+        StringWriter sw = new StringWriter();
+        coreException.printStackTrace( new PrintWriter( sw ) );
+        assertTrue( sw.toString().contains( MSG ) );
+    }
+}
