cbab134639572f01b7aa6dfd7b76180d2d03daa4
emantos <emantos@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2007-09-05 05:02:54 +0000

    Changed the copyright with the ones provided in wiki.

--- plugins/maven/core/src/org/devzuz/q/maven/embedder/internal/EclipseMavenExecutionResult.java
+++ plugins/maven/core/src/org/devzuz/q/maven/embedder/internal/EclipseMavenExecutionResult.java
@@ -1,10 +1,10 @@
-/*******************************************************************************
- * Copyright (c) 2007 Simula Labs
+/*
+ * Copyright (c) 2007 DevZuz, Inc. (AKA Simula Labs, Inc.) and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
+ */
 package org.devzuz.q.maven.embedder.internal;
 
 import java.util.List;
