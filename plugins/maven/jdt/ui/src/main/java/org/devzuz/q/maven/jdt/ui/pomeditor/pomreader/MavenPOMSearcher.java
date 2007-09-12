83a2556ce8f023998a07572339c67ee4bb5b501a
xgenesis007 <xgenesis007@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2007-09-12 03:07:21 +0000

    Disabled POM Editor.

--- plugins/maven/jdt/ui/src/main/java/org/devzuz/q/maven/jdt/ui/pomeditor/pomreader/MavenPOMSearcher.java
+++ plugins/maven/jdt/ui/src/main/java/org/devzuz/q/maven/jdt/ui/pomeditor/pomreader/MavenPOMSearcher.java
@@ -35,8 +35,7 @@ public class MavenPOMSearcher
                         return getPOMFileLocation(project);
                     }
                 }
-            }
-            
+            }            
         }
         return null;
     }
