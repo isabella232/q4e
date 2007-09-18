755d26cdc6a461c0eeb8ebb32bf0461d74bcc66a
emantos <emantos@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2007-09-18 15:13:31 +0000

    Remove unnecessary import.

--- plugins/maven/core/src/org/devzuz/q/maven/embedder/internal/EclipseMaven.java
+++ plugins/maven/core/src/org/devzuz/q/maven/embedder/internal/EclipseMaven.java
@@ -20,7 +20,6 @@ import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
 import org.apache.maven.artifact.resolver.ArtifactResolutionException;
-import org.apache.maven.artifact.resolver.MultipleArtifactsNotFoundException;
 import org.apache.maven.artifact.versioning.ArtifactVersion;
 import org.apache.maven.embedder.Configuration;
 import org.apache.maven.embedder.ConfigurationValidationResult;
