ac5e5134c3d081bf39ccc33f116f314770aa425f
xgenesis007 <xgenesis007@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2007-09-24 11:12:31 +0000

    Allows another url pattern.

--- plugins/maven/ui/src/main/java/org/devzuz/q/maven/ui/dialogs/ArchetypeListSourceDialog.java
+++ plugins/maven/ui/src/main/java/org/devzuz/q/maven/ui/dialogs/ArchetypeListSourceDialog.java
@@ -152,7 +152,7 @@ public class ArchetypeListSourceDialog extends AbstractResizableDialog
         boolean retVal = false;
         try 
         {
-            Pattern pattern = Pattern.compile("^((http)|(https)|(ftp)){1}://[\\p{Alnum}]{3,}+.{1}[\\p{Alnum}]{3}((.)([\\p{Alnum}?+/=-]){1,})*$");
+            Pattern pattern = Pattern.compile("^((http)|(https)|(ftp)){1}://[\\p{Alnum}]{3,}+.{1}[\\p{Alnum}-]{2,}((.)([\\p{Alnum}?+/=-]){1,})*$");
             Matcher m = pattern.matcher(strURL);
             if(m.matches())
                 {
