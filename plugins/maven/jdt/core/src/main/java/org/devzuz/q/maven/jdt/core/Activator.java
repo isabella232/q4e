9966eb17d7f2859a9070cfdda6275bf64a7b8cbf
carlossg <carlossg@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2007-10-22 18:19:04 +0000

    Initializae the date format in the static part

--- plugins/maven/jdt/core/src/main/java/org/devzuz/q/maven/jdt/core/Activator.java
+++ plugins/maven/jdt/core/src/main/java/org/devzuz/q/maven/jdt/core/Activator.java
@@ -38,7 +38,7 @@ public class Activator extends Plugin
     /**
      * Formatter for times displayed in traces.
      */
-    private static DateFormat TIME_FORMAT;
+    private static DateFormat TIME_FORMAT = new SimpleDateFormat( "HH:mm:ss.SSS  " );
 
     /**
      * Marker id
@@ -120,10 +120,6 @@ public class Activator extends Plugin
             String timingValue = Platform.getDebugOption( PLUGIN_GLOBAL_TRACE_OPTION + "/timing" );
             if ( null != timingValue && timingValue.equals( "true" ) )
             {
-                if ( null == TIME_FORMAT )
-                {
-                    TIME_FORMAT = new SimpleDateFormat( "HH:mm:ss.SSS  " );
-                }
                 System.out.print( TIME_FORMAT.format( new Date() ) );
             }
             System.out.print( "[" );
