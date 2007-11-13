d9fa10efdb3f677e5966cac297225d4de16d3c1e
xgenesis007 <xgenesis007@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2007-11-13 02:47:13 +0000

    update for ant to maven conversion

--- plugins/maven/ui/src/main/java/org/devzuz/q/maven/ui/actions/MavenConvertAntAction.java
+++ plugins/maven/ui/src/main/java/org/devzuz/q/maven/ui/actions/MavenConvertAntAction.java
@@ -8,26 +8,94 @@
 
 package org.devzuz.q.maven.ui.actions;
 
+
+import org.devzuz.q.maven.embedder.MavenUtils;
+import org.devzuz.q.maven.ui.actions.helper.MavenConvertProjectHelper;
+
+import org.apache.maven.aardvark.converter.ConversionFailedException;
+import org.apache.maven.aardvark.converter.ProjectConverter;
+import org.apache.maven.aardvark.converter.ant.AntProjectConverter;
+
+import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+
+import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 
-public class MavenConvertAntAction implements IObjectActionDelegate
+import java.io.File;
+import java.io.IOException;
+
+
+public class MavenConvertAntAction 
+    implements IObjectActionDelegate
 {
+
+    private IStructuredSelection selection;
+
+    private static String BUILD_XML = "build.xml";
+    
+
     public void setActivePart( IAction action, IWorkbenchPart targetPart )
     {
-        // TODO
     }
 
     public void run( IAction action )
-    {
-        // TODO
-        
+    {   
+
+        File antBuildFile = getEclipseProject().getFile( BUILD_XML ).getLocation().toFile();
+        PlexusContainer pc = MavenUtils.getPlexusContainer();
+        if(pc != null )
+        {        
+           
+            AntProjectConverter converter;
+            
+            try
+            {                 
+                
+                converter = (AntProjectConverter) pc.lookup( ProjectConverter.ROLE, "ant" );
+                
+                converter.setSrcDirectory( "src/java" ); 
+
+                converter.convertFromPath(antBuildFile.getParentFile(), 
+                                          antBuildFile.getParentFile());
+                
+            }
+            catch ( ComponentLookupException e )
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }  
+            catch ( IOException e )
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch ( ConversionFailedException e )
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }           
+
+        }
+
     }
 
     public void selectionChanged( IAction action, ISelection selection )
     {
-        // TODO
+        if(!selection.isEmpty())
+        {
+            this.selection = (IStructuredSelection) selection;
+        }
     }
+    
+    private IProject getEclipseProject()
+    {
+        return MavenConvertProjectHelper.getEclipseProject( this.selection );
+    }
+    
 }
+
