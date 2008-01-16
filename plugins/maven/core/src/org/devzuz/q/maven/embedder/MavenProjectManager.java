96429081b610341cc636e76122e2f697a613f052
amuino <amuino@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-01-16 21:08:25 +0000

    Check for null persistent project properties (the EclipseArtifactResolver can invoke this code before a project has been imported but after it has been placed in the map).

--- plugins/maven/core/src/org/devzuz/q/maven/embedder/MavenProjectManager.java
+++ plugins/maven/core/src/org/devzuz/q/maven/embedder/MavenProjectManager.java
@@ -16,18 +16,18 @@ import org.eclipse.core.runtime.CoreException;
 
 public class MavenProjectManager
 {
-    private Map< IProject , MavenProjectCachedInfo > mavenProjects;
-    
+    private Map<IProject, MavenProjectCachedInfo> mavenProjects;
+
     public MavenProjectManager()
     {
         // TODO : Should this be a synchronized collection ?
         // mavenProjects = Collections.synchronizedMap( new HashMap< IProject , MavenProjectCachedInfo >() );
-        mavenProjects = new HashMap< IProject , MavenProjectCachedInfo >();
+        mavenProjects = new HashMap<IProject, MavenProjectCachedInfo>();
     }
-    
+
     /**
-     * This constructor initializes the cache with the current maven projects in the workspace.
-     * This maven projects are not yet resolved and will be resolved when actually needed.
+     * This constructor initializes the cache with the current maven projects in the workspace. This maven projects are
+     * not yet resolved and will be resolved when actually needed.
      * 
      * @param workspace
      * @throws CoreException
@@ -44,99 +44,105 @@ public class MavenProjectManager
             }
         }
     }
-    
+
     /**
-     * This method will add the given project to the cache with a null IMavenProject
-     * resolvedTransitively set to false
+     * This method will add the given project to the cache with a null IMavenProject resolvedTransitively set to false
      * 
-     * Use this method if the project is in the workspace but you want to
-     * resolve its IMavenProject only when it's really needed.
+     * Use this method if the project is in the workspace but you want to resolve its IMavenProject only when it's
+     * really needed.
      * 
-     * @param project The IProject this project represents in the workspace
+     * @param project
+     *            The IProject this project represents in the workspace
      */
     public void addMavenProject( IProject project )
     {
-        addCachedInfo( project , null );
+        addCachedInfo( project, null );
     }
-    
+
     /**
-     * This method will add the given project and mavenProject to the cache with 
-     * resolvedTransitively set to false
+     * This method will add the given project and mavenProject to the cache with resolvedTransitively set to false
      * 
-     * @param project The IProject this project represents in the workspace
-     * @param mavenProject The IMavenProject associated with the IProject
+     * @param project
+     *            The IProject this project represents in the workspace
+     * @param mavenProject
+     *            The IMavenProject associated with the IProject
      */
-    public void addMavenProject( IProject project , IMavenProject mavenProject )
+    public void addMavenProject( IProject project, IMavenProject mavenProject )
     {
-        addMavenProject( project , mavenProject , false );
+        addMavenProject( project, mavenProject, false );
     }
-    
+
     /**
      * This method will add the given project and mavenProject to the cache
      * 
-     * @param project The IProject this project represents in the workspace
-     * @param mavenProject The IMavenProject associated with the IProject
-     * @param resolvedTransitively If this IMavenProject was resolved transitively
+     * @param project
+     *            The IProject this project represents in the workspace
+     * @param mavenProject
+     *            The IMavenProject associated with the IProject
+     * @param resolvedTransitively
+     *            If this IMavenProject was resolved transitively
      */
-    public void addMavenProject( IProject project , IMavenProject mavenProject , boolean resolvedTransitively )
+    public void addMavenProject( IProject project, IMavenProject mavenProject, boolean resolvedTransitively )
     {
-        MavenProjectCachedInfo cachedProject =  MavenProjectCachedInfo.newMavenProjectCachedInfo( mavenProject , resolvedTransitively );
+        MavenProjectCachedInfo cachedProject =
+            MavenProjectCachedInfo.newMavenProjectCachedInfo( mavenProject, resolvedTransitively );
         addCachedInfo( project, cachedProject );
     }
-    
+
     /**
      * Remove the given IProject from the cache
      * 
-     * @param project The IProject this project represents in the workspace
+     * @param project
+     *            The IProject this project represents in the workspace
      */
     public void removeMavenProject( IProject project )
     {
         mavenProjects.remove( project );
     }
-    
+
     /**
-     * Sets the given IProject from the cache to a modified state. This has the effect of
-     * the IMavenProject getting renewed from the maven embedder if getMavenProject(...) is called on it.
+     * Sets the given IProject from the cache to a modified state. This has the effect of the IMavenProject getting
+     * renewed from the maven embedder if getMavenProject(...) is called on it.
      * 
-     * @param project The IProject this project represents in the workspace
+     * @param project
+     *            The IProject this project represents in the workspace
      */
     public void setMavenProjectModified( IProject project )
     {
         MavenProjectCachedInfo cachedProject = mavenProjects.get( project );
-        if( cachedProject != null )
+        if ( cachedProject != null )
         {
             cachedProject.setModified( true );
         }
     }
-    
+
     /**
      * Retrieves the IMavenProject for the given IProject from the cache or the maven embedder following a set of rules.
      * 
-     * @param project The IProject this project represents in the workspace
-     * @param resolveTransitively Should it be retrieved with transitive dependencies resolved?
+     * @param project
+     *            The IProject this project represents in the workspace
+     * @param resolveTransitively
+     *            Should it be retrieved with transitive dependencies resolved?
      * @return The maven project
      * @throws CoreException
      */
-    public IMavenProject getMavenProject( IProject project , boolean resolveTransitively ) 
-        throws CoreException
+    public IMavenProject getMavenProject( IProject project, boolean resolveTransitively ) throws CoreException
     {
         MavenProjectCachedInfo cachedProject = mavenProjects.get( project );
-        
-        // If we haven't cached the project yet or if the maven project's pom was modified or 
+
+        // If we haven't cached the project yet or if the maven project's pom was modified or
         // if we have cached the project but it's not resolved transitively and resolveTransitively is true
-        if( ( cachedProject == null ) ||
-            ( cachedProject.isModified() ) ||
-            ( ( cachedProject.resolvedTransitively == false ) && 
-              ( resolveTransitively == true ) ) )
+        if ( ( cachedProject == null ) || ( cachedProject.isModified() )
+                        || ( ( cachedProject.resolvedTransitively == false ) && ( resolveTransitively == true ) ) )
         {
-            IMavenProject mavenProject = MavenManager.getMaven().getMavenProject( project , resolveTransitively );
-            cachedProject =  MavenProjectCachedInfo.newMavenProjectCachedInfo( mavenProject , resolveTransitively );
+            IMavenProject mavenProject = MavenManager.getMaven().getMavenProject( project, resolveTransitively );
+            cachedProject = MavenProjectCachedInfo.newMavenProjectCachedInfo( mavenProject, resolveTransitively );
             addCachedInfo( project, cachedProject );
         }
-        
+
         return cachedProject.getMavenProject();
     }
-    
+
     /**
      * Retrieves the IMavenProject for the given IProject from the cache or the maven embedder following a set of rules.
      * 
@@ -146,24 +152,25 @@ public class MavenProjectManager
      * @return The maven project
      * @throws CoreException
      */
-    public IMavenProject getMavenProject( String groupId, String artifactId , String version , boolean resolveTransitively ) 
+    public IMavenProject getMavenProject( String groupId, String artifactId, String version, boolean resolveTransitively )
         throws CoreException
     {
-        Map.Entry< IProject , MavenProjectCachedInfo > entry = getEntry( groupId, artifactId, version );
-        if( entry != null )
+        Map.Entry<IProject, MavenProjectCachedInfo> entry = getEntry( groupId, artifactId, version );
+        if ( entry != null )
         {
-            if( entry.getValue() == null )
+            if ( entry.getValue() == null )
             {
-                return getMavenProject( entry.getKey() , resolveTransitively );
+                return getMavenProject( entry.getKey(), resolveTransitively );
             }
             else
             {
                 return entry.getValue().getMavenProject();
             }
         }
-             
+
         return null;
     }
+
     /**
      * Retrieve the IProject for the given triplet
      * 
@@ -172,88 +179,96 @@ public class MavenProjectManager
      * @param version
      * @return the workspace project
      */
-    public IProject getWorkspaceProject( String groupId, String artifactId , String version )
+    public IProject getWorkspaceProject( String groupId, String artifactId, String version )
     {
-        Map.Entry< IProject , MavenProjectCachedInfo > info = getEntry( groupId, artifactId, version );
-        if( info != null )
+        Map.Entry<IProject, MavenProjectCachedInfo> info = getEntry( groupId, artifactId, version );
+        if ( info != null )
         {
             return info.getKey();
         }
-        
+
         return null;
     }
-    
+
     /**
      * Retrieve all the IProject's in the cache
+     * 
      * @return all the cached workspace project
      */
     public IProject[] getWorkspaceProjects()
     {
-        return mavenProjects.keySet().toArray( new IProject[ mavenProjects.size() ] );
+        return mavenProjects.keySet().toArray( new IProject[mavenProjects.size()] );
     }
-    
+
     private void addCachedInfo( IProject project, MavenProjectCachedInfo cachedProject )
     {
-        // TODO : Do we need to explicitly set the old value contained therein to null so it could be garbage collected ?
-        MavenProjectCachedInfo oldInfo = mavenProjects.put( project , cachedProject );
-        if( oldInfo != null )
+        // TODO : Do we need to explicitly set the old value contained therein to null so it could be garbage collected
+        // ?
+        MavenProjectCachedInfo oldInfo = mavenProjects.put( project, cachedProject );
+        if ( oldInfo != null )
         {
             oldInfo = null;
         }
     }
-    
-    private Map.Entry< IProject , MavenProjectCachedInfo > getEntry( String groupId, String artifactId , String version )
+
+    private Map.Entry<IProject, MavenProjectCachedInfo> getEntry( String groupId, String artifactId, String version )
     {
-        for( Map.Entry< IProject , MavenProjectCachedInfo > entry : mavenProjects.entrySet() )
+        for ( Map.Entry<IProject, MavenProjectCachedInfo> entry : mavenProjects.entrySet() )
         {
             try
             {
                 IProject project = entry.getKey();
-                
+
                 String _groupId = project.getPersistentProperty( IMavenProject.GROUP_ID );
                 String _artifactId = project.getPersistentProperty( IMavenProject.ARTIFACT_ID );
                 String _version = project.getPersistentProperty( IMavenProject.VERSION );
-                
-                if( _groupId.equals( groupId ) && 
-                    _artifactId.equals( artifactId ) &&
-                    _version.equals( version ) )
+
+                // The artifact resolver executes this loop before the project has been created, so ALWAYS skip maven
+                // projects without persistent properties.
+                if ( _groupId != null && _groupId.equals( groupId ) && _artifactId.equals( artifactId )
+                                && _version.equals( version ) )
                 {
                     return entry;
                 }
             }
             catch ( CoreException e )
             {
-                MavenCoreActivator.getLogger().error( "Can't read triplet from project's '" + entry.getKey().getName() + "' persistent properties" );
+                MavenCoreActivator.getLogger().error(
+                                                      "Can't read triplet from project's '" + entry.getKey().getName()
+                                                                      + "' persistent properties" );
             }
         }
-        
+
         return null;
     }
-    
+
     private static class MavenProjectCachedInfo
     {
         private IMavenProject mavenProject;
-        private boolean       resolvedTransitively = false;
-        private boolean       modified = false;
-        
-        public static MavenProjectCachedInfo newMavenProjectCachedInfo( IMavenProject mavenProject , boolean resolvedTransitively )
+
+        private boolean resolvedTransitively = false;
+
+        private boolean modified = false;
+
+        public static MavenProjectCachedInfo newMavenProjectCachedInfo( IMavenProject mavenProject,
+                                                                        boolean resolvedTransitively )
         {
             // TODO : add digest so we could detect if mavenProject's POM has changed
-            return new MavenProjectCachedInfo( mavenProject , resolvedTransitively , false );
+            return new MavenProjectCachedInfo( mavenProject, resolvedTransitively, false );
         }
-        
-        private MavenProjectCachedInfo( IMavenProject project , boolean resolvedTransitively, boolean modified )
+
+        private MavenProjectCachedInfo( IMavenProject project, boolean resolvedTransitively, boolean modified )
         {
             this.mavenProject = project;
             this.resolvedTransitively = resolvedTransitively;
             this.modified = modified;
         }
-        
+
         public IMavenProject getMavenProject()
         {
             return mavenProject;
         }
-        
+
         public void setMavenProject( IMavenProject mavenProject )
         {
             this.mavenProject = mavenProject;
