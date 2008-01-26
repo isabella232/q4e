7f8ad1591684a0c2c2688f1ce28bb6e3b35b281a
carlossg <carlossg@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-01-26 03:02:27 +0000

    During creation set the values from the preferences page

--- plugins/maven/core/src/org/devzuz/q/maven/embedder/MavenExecutionParameter.java
+++ plugins/maven/core/src/org/devzuz/q/maven/embedder/MavenExecutionParameter.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2007-2008 DevZuz, Inc. (AKA Simula Labs, Inc.) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.devzuz.q.maven.embedder;
 
 import java.util.Properties;
@@ -24,9 +31,12 @@ public class MavenExecutionParameter
     
     public static MavenExecutionParameter newDefaultMavenExecutionParameter()
     {
-        return new MavenExecutionParameter(); 
+        MavenExecutionParameter parameter = new MavenExecutionParameter();
+        parameter.setOffline( MavenManager.getMavenPreferenceManager().isOffline() );
+        parameter.setRecursive( MavenManager.getMavenPreferenceManager().isRecursive() );
+        return parameter;
     }
-    
+
     public static MavenExecutionParameter newDefaultMavenExecutionParameter( Properties properties )
     {
         return new MavenExecutionParameter( properties ); 
