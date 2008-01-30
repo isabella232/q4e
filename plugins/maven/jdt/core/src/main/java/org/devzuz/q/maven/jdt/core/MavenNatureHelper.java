c51a6ac32ade2209b42fd60ba867d11524e6073b
carlossg <carlossg@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-01-30 18:47:24 +0000

    Most of the operations should check for the Maven nature instead of only if the pom is present

--- plugins/maven/jdt/core/src/main/java/org/devzuz/q/maven/jdt/core/MavenNatureHelper.java
+++ plugins/maven/jdt/core/src/main/java/org/devzuz/q/maven/jdt/core/MavenNatureHelper.java
@@ -6,60 +6,60 @@
  **************************************************************************************************/
 package org.devzuz.q.maven.jdt.core;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.JavaCore;
 
+/**
+ * Utility to handle the Q4E and JDT natures in a project.
+ * 
+ * @TODO leave here only JDT specific aspects and refactor for other languages
+ */
 public class MavenNatureHelper
+    extends org.devzuz.q.maven.embedder.nature.MavenNatureHelper
 {
 
-    private static String NATURE_ID = MavenJdtCoreActivator.PLUGIN_ID + ".mavenNature";
+    private static final MavenNatureHelper INSTANCE = new MavenNatureHelper();
 
-    public static void addNature( IProject project ) throws CoreException
+    protected MavenNatureHelper()
     {
-        Set<String> newNatures = new HashSet<String>();
-        newNatures.add( JavaCore.NATURE_ID );
-        newNatures.add( NATURE_ID );
-
-        IProjectDescription description = project.getDescription();
-        HashSet<String> existingNatureIds = new HashSet<String>( Arrays.asList( description.getNatureIds() ) );
-        existingNatureIds.addAll( newNatures );
-        description.setNatureIds( existingNatureIds.toArray( new String[existingNatureIds.size()] ) );
-        if ( description != null )
-        {
-            project.setDescription( description, null );
-        }
     }
 
-    public static void removeNature( IProject project ) throws CoreException
+    public static MavenNatureHelper getInstance()
     {
-        IProjectDescription description = project.getDescription();
-        HashSet<String> existingNatureIds = new HashSet<String>( Arrays.asList( description.getNatureIds() ) );
-        existingNatureIds.remove( NATURE_ID );
-        description.setNatureIds( existingNatureIds.toArray( new String[existingNatureIds.size()] ) );
-        if ( description != null )
-        {
-            project.setDescription( description, null );
-        }
+        return INSTANCE;
     }
 
     /**
-     * Helper method for checking if a given project has the <b>q4e</b> nature enabled.
+     * Adds the Q4E and JDT nature to the project
      * 
      * @param project
-     *            the project to check.
-     * @return <code>true</code> if the maven has q4e enabled.
      * @throws CoreException
-     *             if the natures of the project can't be read.
      */
-    public static boolean hasMavenNature( IProject project ) throws CoreException
+    public static void addNature( IProject project )
+        throws CoreException
     {
-        IProjectDescription description = project.getDescription();
-        return description.hasNature( NATURE_ID );
+        Set<String> newNatures = new HashSet<String>();
+        newNatures.add( JavaCore.NATURE_ID );
+        newNatures.add( org.devzuz.q.maven.embedder.nature.MavenNatureHelper.NATURE_ID );
+        getInstance().addNatures( project, newNatures );
+    }
+
+    public static void removeNature( IProject project )
+        throws CoreException
+    {
+        getInstance().removeQ4ENature( project );
+    }
+
+    /**
+     * @see #hasQ4ENature(IProject)
+     */
+    public static boolean hasMavenNature( IProject project )
+        throws CoreException
+    {
+        return getInstance().hasQ4ENature( project );
     }
 }
