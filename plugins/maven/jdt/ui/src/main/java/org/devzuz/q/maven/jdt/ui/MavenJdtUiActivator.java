c1c1762569c100e232b15f8888332e13c4491b85
amuino <amuino@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-02-04 22:53:55 +0000

    Implement basic plumbing for traces in maven jdt ui.

--- plugins/maven/jdt/ui/src/main/java/org/devzuz/q/maven/jdt/ui/MavenJdtUiActivator.java
+++ plugins/maven/jdt/ui/src/main/java/org/devzuz/q/maven/jdt/ui/MavenJdtUiActivator.java
@@ -6,20 +6,35 @@
  **************************************************************************************************/
 package org.devzuz.q.maven.jdt.ui;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.devzuz.q.maven.embedder.log.EclipseLogger;
 import org.devzuz.q.maven.embedder.log.Logger;
+import org.devzuz.q.maven.jdt.ui.internal.TraceOption;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class MavenJdtUiActivator
-    extends AbstractUIPlugin
+public class MavenJdtUiActivator extends AbstractUIPlugin
 {
 
     /** The plug-in ID */
     public static final String PLUGIN_ID = "org.devzuz.q.maven.jdt.ui";
+
+    /**
+     * Prefix string for all trace options
+     */
+    public static final String PLUGIN_GLOBAL_TRACE_OPTION = PLUGIN_ID + "/debug";
+
+    /**
+     * Formatter for times displayed in traces.
+     */
+    private static DateFormat TIME_FORMAT;
 
     /** The shared instance */
     private static MavenJdtUiActivator plugin;
@@ -33,16 +48,16 @@ public class MavenJdtUiActivator
     {
     }
 
-    public void start( BundleContext context )
-        throws Exception
+    @Override
+    public void start( BundleContext context ) throws Exception
     {
         super.start( context );
         logger = new EclipseLogger( PLUGIN_ID, this.getLog() );
         plugin = this;
     }
 
-    public void stop( BundleContext context )
-        throws Exception
+    @Override
+    public void stop( BundleContext context ) throws Exception
     {
         plugin = null;
         super.stop( context );
@@ -63,4 +78,48 @@ public class MavenJdtUiActivator
         return getDefault().logger;
     }
 
+    /**
+     * Sends trace messages to stdout if the specified trace option is enabled.
+     * 
+     * This is intended only for developing and debugging. The use of variable number of parameters avoids the cost of
+     * building the message when the debug option is not enabled.
+     * 
+     * @param traceOption
+     *            the trace option enabling the message trace.
+     * @param messageParts
+     *            a variable number of objects with each part of the message to display.
+     */
+    public static void trace( TraceOption traceOption, Object... messageParts )
+    {
+        if ( !Platform.inDebugMode() )
+        {
+            return;
+        }
+        String globalTraceValue = Platform.getDebugOption( PLUGIN_GLOBAL_TRACE_OPTION );
+        if ( null == globalTraceValue || !globalTraceValue.equals( "true" ) )
+        {
+            return;
+        }
+        String value = Platform.getDebugOption( traceOption.getValue() );
+        if ( null != value && value.equals( "true" ) )
+        {
+            String timingValue = Platform.getDebugOption( PLUGIN_GLOBAL_TRACE_OPTION + "/timing" );
+            if ( null != timingValue && timingValue.equals( "true" ) )
+            {
+                if ( null == TIME_FORMAT )
+                {
+                    TIME_FORMAT = new SimpleDateFormat( "HH:mm:ss.SSS  " );
+                }
+                System.out.print( TIME_FORMAT.format( new Date() ) );
+            }
+            System.out.print( "[" );
+            System.out.print( traceOption );
+            System.out.print( "] " );
+            for ( int i = 0; i < messageParts.length; i++ )
+            {
+                System.out.print( messageParts[i] );
+            }
+            System.out.println();
+        }
+    }
 }
---
+++ plugins/maven/jdt/ui/src/main/java/org/devzuz/q/maven/jdt/ui/internal/TraceOption.java
@@ -0,0 +1,33 @@
+package org.devzuz.q.maven.jdt.ui.internal;
+
+import org.devzuz.q.maven.jdt.ui.MavenJdtUiActivator;
+
+/**
+ * This enumeration contains the constants used to enable traces for the plug-in.
+ * 
+ * @author amuino
+ */
+public enum TraceOption
+{
+    PROJECT_IMPORT( "/import" );
+
+    private final String value;
+
+    TraceOption( String value )
+    {
+        this.value = MavenJdtUiActivator.PLUGIN_GLOBAL_TRACE_OPTION + value;
+    }
+
+    /**
+     * Obtains the string value of the trace option, including the plug-in ID and global debug prefix.
+     * 
+     * For example: <code>org.devzuz.q.maven.jdt.ui/debug/import</code>
+     * 
+     * @return the string value of the trace option.
+     */
+    public String getValue()
+    {
+        return value;
+    }
+
+}
