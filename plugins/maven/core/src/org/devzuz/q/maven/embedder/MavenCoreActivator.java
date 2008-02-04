6730c77f704736b788138511505291ac29814334
amuino <amuino@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-02-04 22:53:28 +0000

    Implement basic plumbing for traces in maven core.

--- plugins/maven/core/src/org/devzuz/q/maven/embedder/MavenCoreActivator.java
+++ plugins/maven/core/src/org/devzuz/q/maven/embedder/MavenCoreActivator.java
@@ -7,11 +7,17 @@
  *******************************************************************************/
 package org.devzuz.q.maven.embedder;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.devzuz.q.maven.embedder.internal.EclipseMaven;
+import org.devzuz.q.maven.embedder.internal.TraceOption;
 import org.devzuz.q.maven.embedder.log.EclipseLogger;
 import org.devzuz.q.maven.embedder.log.Logger;
 import org.eclipse.core.internal.runtime.Log;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
 import org.osgi.framework.BundleActivator;
@@ -25,6 +31,16 @@ public class MavenCoreActivator implements BundleActivator
 
     // The plug-in ID
     public static final String PLUGIN_ID = "org.devzuz.q.maven.core";
+
+    /**
+     * Prefix string for all trace options
+     */
+    public static final String PLUGIN_GLOBAL_TRACE_OPTION = PLUGIN_ID + "/debug";
+
+    /**
+     * Formatter for times displayed in traces.
+     */
+    private static DateFormat TIME_FORMAT;
 
     // The shared instance
     private static MavenCoreActivator plugin;
@@ -101,5 +117,50 @@ public class MavenCoreActivator implements BundleActivator
     public static Logger getLogger()
     {
         return getDefault().logger;
+    }
+
+    /**
+     * Sends trace messages to stdout if the specified trace option is enabled.
+     * 
+     * This is intended only for developing and debugging. The use of variable number of parameters avoids the cost of
+     * building the message when the debug option is not enabled.
+     * 
+     * @param traceOption
+     *            the trace option enabling the message trace.
+     * @param messageParts
+     *            a variable number of objects with each part of the message to display.
+     */
+    public static void trace( TraceOption traceOption, Object... messageParts )
+    {
+        if ( !Platform.inDebugMode() )
+        {
+            return;
+        }
+        String globalTraceValue = Platform.getDebugOption( PLUGIN_GLOBAL_TRACE_OPTION );
+        if ( null == globalTraceValue || !globalTraceValue.equals( "true" ) )
+        {
+            return;
+        }
+        String value = Platform.getDebugOption( traceOption.getValue() );
+        if ( null != value && value.equals( "true" ) )
+        {
+            String timingValue = Platform.getDebugOption( PLUGIN_GLOBAL_TRACE_OPTION + "/timing" );
+            if ( null != timingValue && timingValue.equals( "true" ) )
+            {
+                if ( null == TIME_FORMAT )
+                {
+                    TIME_FORMAT = new SimpleDateFormat( "HH:mm:ss.SSS  " );
+                }
+                System.out.print( TIME_FORMAT.format( new Date() ) );
+            }
+            System.out.print( "[" );
+            System.out.print( traceOption );
+            System.out.print( "] " );
+            for ( int i = 0; i < messageParts.length; i++ )
+            {
+                System.out.print( messageParts[i] );
+            }
+            System.out.println();
+        }
     }
 }
