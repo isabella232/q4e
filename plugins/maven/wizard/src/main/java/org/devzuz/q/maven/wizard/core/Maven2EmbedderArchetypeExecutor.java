4a29a11f9e959fda4a15cdc6c9efa81c43434f0b
carlossg <carlossg@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-02-06 19:37:15 +0000

    [223] Add archetype plugin version and other info to the preference manager

--- plugins/maven/wizard/src/main/java/org/devzuz/q/maven/wizard/core/Maven2EmbedderArchetypeExecutor.java
+++ plugins/maven/wizard/src/main/java/org/devzuz/q/maven/wizard/core/Maven2EmbedderArchetypeExecutor.java
@@ -1,24 +1,28 @@
-/***************************************************************************************************
- * Copyright (c) 2007 DevZuz, Inc. (AKA Simula Labs, Inc.) All rights reserved. This program and the
- * accompanying materials are made available under the terms of the Eclipse Public License v1.0
+/*
+ * Copyright (c) 2007-2008 DevZuz, Inc. (AKA Simula Labs, Inc.) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- **************************************************************************************************/
+ */
 package org.devzuz.q.maven.wizard.core;
 
 import java.util.Properties;
 
+import org.devzuz.q.maven.embedder.MavenCoreActivator;
 import org.devzuz.q.maven.embedder.MavenExecutionJobAdapter;
 import org.devzuz.q.maven.embedder.MavenExecutionParameter;
 import org.devzuz.q.maven.embedder.MavenManager;
+import org.devzuz.q.maven.embedder.MavenPreferenceManager;
 import org.devzuz.q.maven.ui.archetype.provider.Archetype;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 
-public class Maven2EmbedderArchetypeExecutor implements IArchetypeExecutor
+public class Maven2EmbedderArchetypeExecutor
+    implements IArchetypeExecutor
 {
     public void executeArchetype( Archetype archetype, IPath baseDir, String groupId, String artifactId,
-                                  String version, String packageName, IMavenWizardContext wizardContext ,
+                                  String version, String packageName, IMavenWizardContext wizardContext,
                                   MavenExecutionJobAdapter jobAdapter )
         throws CoreException
     {
@@ -32,22 +36,39 @@ public class Maven2EmbedderArchetypeExecutor implements IArchetypeExecutor
         archetypeProperties.setProperty( "archetypeGroupId", archetype.getGroupId() );
         archetypeProperties.setProperty( "basedir", baseDir.makeAbsolute().toOSString() );
 
-        if ( !archetype.getVersion().equals( "" ) )
+        if ( !archetype.getVersion().isEmpty() )
             archetypeProperties.setProperty( "archetypeVersion", archetype.getVersion() );
 
-        if ( !archetype.getRemoteRepositories().equals( "" ) )
+        if ( !archetype.getRemoteRepositories().isEmpty() )
             archetypeProperties.setProperty( "remoteRepositories", archetype.getRemoteRepositories() );
-        
-        if( jobAdapter == null )
+
+        if ( jobAdapter == null )
         {
-            MavenManager.getMaven().scheduleGoal( baseDir, ARCHETYPE_PLUGIN_ID + ":create",
+            MavenManager.getMaven().scheduleGoal(
+                                                  baseDir,
+                                                  getArchetypePluginCreationId(),
                                                   MavenExecutionParameter.newDefaultMavenExecutionParameter( archetypeProperties ) );
         }
         else
         {
-            MavenManager.getMaven().scheduleGoal( baseDir, ARCHETYPE_PLUGIN_ID + ":create",
+            MavenManager.getMaven().scheduleGoal(
+                                                  baseDir,
+                                                  getArchetypePluginCreationId(),
                                                   MavenExecutionParameter.newDefaultMavenExecutionParameter( archetypeProperties ),
                                                   jobAdapter );
         }
+    }
+
+    private String getArchetypePluginCreationId()
+    {
+        StringBuilder sb = new StringBuilder();
+        MavenPreferenceManager preferenceManager = MavenCoreActivator.getDefault().getMavenPreferenceManager();
+        sb.append( preferenceManager.getArchetypePluginGroupId() );
+        sb.append( ":" );
+        sb.append( preferenceManager.getArchetypePluginArtifactId() );
+        sb.append( ":" );
+        sb.append( preferenceManager.getArchetypePluginVersion() );
+        sb.append( ":create" );
+        return sb.toString();
     }
 }
