be00b855d17cd498f10ceb15f0795d8ff1c3efdc
carlossg <carlossg@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-02-06 22:56:51 +0000

    [144] plugin disables itself in the menu when used on a non-maven project plugin provides a clearer error response when user tries to analyse a broken maven project Apply patch from Jake Pezaro

--- plugins/maven/dependency-analysis/src/main/java/org/devzuz/q/maven/dependency/analysis/actions/AnalyseDependencyAction.java
+++ plugins/maven/dependency-analysis/src/main/java/org/devzuz/q/maven/dependency/analysis/actions/AnalyseDependencyAction.java
@@ -6,13 +6,18 @@
  **************************************************************************************************/
 package org.devzuz.q.maven.dependency.analysis.actions;
 
+import org.devzuz.q.maven.dependency.analysis.DependencyAnalysisActivator;
 import org.devzuz.q.maven.dependency.analysis.threads.ResolveDependenciesJob;
 import org.devzuz.q.maven.embedder.IMavenProject;
 import org.devzuz.q.maven.embedder.MavenManager;
 import org.devzuz.q.maven.ui.actions.AbstractMavenAction;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
 /**
  * Action which displays the dependency analyser graph for the selected maven project.
@@ -29,8 +34,24 @@ public class AnalyseDependencyAction
     {
 
         IMavenProject mavenProject = getMavenProject();
-        ResolveDependenciesJob resolver =
-            new ResolveDependenciesJob( mavenProject, MavenManager.getMaven(), Display.getCurrent() );
-        resolver.schedule();
+
+        if ( mavenProject == null )
+        {
+            // Need to explain to the user that their requested action has failed and why
+            ErrorDialog error =
+                new ErrorDialog( new Shell( Display.getCurrent() ), "Maven Dependency Analysis Error",
+                                 "Project is not a valid maven project",
+                                 new Status( Status.ERROR, DependencyAnalysisActivator.PLUGIN_ID,
+                                             "Dependency analysis can only be used on valid maven projects" ),
+                                 IStatus.ERROR );
+            error.open();
+        }
+        else
+        {
+
+            ResolveDependenciesJob resolver =
+                new ResolveDependenciesJob( mavenProject, MavenManager.getMaven(), Display.getCurrent() );
+            resolver.schedule();
+        }
     }
 }
