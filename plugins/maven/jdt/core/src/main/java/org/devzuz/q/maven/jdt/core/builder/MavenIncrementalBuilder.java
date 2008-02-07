49d7811c0fc34ffe090f31bdd9e9298625a556a4
amuino <amuino@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-02-07 21:26:04 +0000

    Apply includes/excludes to resource folders (allows sharing a folder for classes and resources).

--- plugins/maven/jdt/core/src/main/java/org/devzuz/q/maven/jdt/core/builder/MavenIncrementalBuilder.java
+++ plugins/maven/jdt/core/src/main/java/org/devzuz/q/maven/jdt/core/builder/MavenIncrementalBuilder.java
@@ -7,10 +7,16 @@
 package org.devzuz.q.maven.jdt.core.builder;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
+import java.util.Queue;
 
 import org.apache.maven.model.Resource;
+import org.apache.tools.ant.types.selectors.SelectorUtils;
 import org.devzuz.q.maven.embedder.IMavenProject;
 import org.devzuz.q.maven.embedder.MavenExecutionParameter;
 import org.devzuz.q.maven.embedder.MavenManager;
@@ -35,10 +41,8 @@ import org.eclipse.core.runtime.Path;
  * Maven builder that will update the classpath container when pom changes
  * 
  * @author <a href="mailto:carlos@apache.org">Carlos Sanchez</a>
- * @version $Id$
  */
-public class MavenIncrementalBuilder
-    extends IncrementalProjectBuilder
+public class MavenIncrementalBuilder extends IncrementalProjectBuilder
 {
 
     public static final String MAVEN_INCREMENTAL_BUILDER_ID =
@@ -47,8 +51,7 @@ public class MavenIncrementalBuilder
     private static final Path POM_PATH = new Path( IMavenProject.POM_FILENAME );
 
     @Override
-    protected IProject[] build( int kind, Map args, IProgressMonitor monitor )
-        throws CoreException
+    protected IProject[] build( int kind, Map args, IProgressMonitor monitor ) throws CoreException
     {
         if ( ( kind == INCREMENTAL_BUILD ) || ( kind == AUTO_BUILD ) )
         {
@@ -66,16 +69,7 @@ public class MavenIncrementalBuilder
 
             if ( mavenProject != null )
             {
-                List<Resource> resources = mavenProject.getResources();
-                if ( deltaContainsResource( delta, resources ) )
-                {
-                    onResourcesChange( mavenProject, "resources:resources" );
-                }
-                List<Resource> testResources = mavenProject.getTestResources();
-                if ( deltaContainsResource( delta, testResources ) )
-                {
-                    onResourcesChange( mavenProject, "resources:testResources" );
-                }
+                handleAllResources( mavenProject, delta );
                 // TODO: if <filter> is set on the pom.xml, the referenced file could have changed
             }
         }
@@ -114,28 +108,176 @@ public class MavenIncrementalBuilder
     }
 
     /**
+     * Handles updates to the generated resources and test resources when needed.
+     * 
      * @param mavenProject
-     * @param goal
+     *            the project to update.
      * @throws CoreException
+     *             if there is a problem updating the resources.
      */
-    private void onResourcesChange( IMavenProject mavenProject, String goal )
-        throws CoreException
+    private void handleAllResources( IMavenProject mavenProject, IResourceDelta delta ) throws CoreException
     {
-        MavenJdtCoreActivator.trace( TraceOption.MAVEN_INCREMENTAL_BUILDER, "Processing resources on ", getProject(),
-                                     " : ", goal );
-        MavenExecutionParameter params = MavenExecutionParameter.newDefaultMavenExecutionParameter();
-        params.setRecursive( false );
-        MavenManager.getMaven().scheduleGoal( mavenProject, goal, params );
+        List<Resource> resources = mavenProject.getResources();
+        List<Resource> testResources = mavenProject.getTestResources();
+        doHandleResources( mavenProject, delta, resources, "resources:resources" );
+        doHandleResources( mavenProject, delta, testResources, "resources:testResources" );
     }
 
     /**
-     * @param testResources
+     * Utility method for factoring checking and refreshing of resources and testResources.
+     * 
+     * @param mavenProject
+     *            the project being modified.
+     * @param delta
+     *            the changes being processed.
+     * @param resources
+     *            the list of resources declared in the pom. Can be either resources or testResources.
+     * @throws CoreException
+     *             if there is a problem updating the resources.
+     */
+    private void doHandleResources( IMavenProject mavenProject, IResourceDelta delta, List<Resource> resources,
+                                    String goal ) throws CoreException
+    {
+        Map<Resource, IPath> resourcePathMap = getPathForResources( mavenProject, resources );
+        for ( Map.Entry<Resource, IPath> e : resourcePathMap.entrySet() )
+        {
+            IResourceDelta deltaMember = delta.findMember( e.getValue() );
+            if ( deltaMember != null )
+            {
+                Resource mavenResource = e.getKey();
+                List<String> allPaths = getAllPaths( deltaMember );
+                MavenJdtCoreActivator.trace( TraceOption.MAVEN_INCREMENTAL_BUILDER, "All affected resources: ",
+                                             allPaths );
+                filterExclusions( allPaths, mavenResource.getExcludes() );
+                MavenJdtCoreActivator.trace( TraceOption.MAVEN_INCREMENTAL_BUILDER, "Affected after excludes: ",
+                                             mavenResource.getExcludes(), "->", allPaths );
+                filterInclusions( allPaths, mavenResource.getIncludes() );
+                MavenJdtCoreActivator.trace( TraceOption.MAVEN_INCREMENTAL_BUILDER, "Affected after includes: ",
+                                             mavenResource.getIncludes(), "->", allPaths );
+                if ( !allPaths.isEmpty() )
+                {
+                    onResourcesChange( mavenProject, goal );
+                }
+            }
+        }
+    }
+
+    /**
+     * Modifies the <code>resourcePaths</code> list removing all the paths matched by the exclusion patterns.
+     * 
+     * If there are no exclusion patterns, the original list is not modified.
+     * 
+     * @param resourcePaths
+     *            the original resource paths for every modified resources in a resource folder.
+     * @param patterns
+     *            the exclusion patterns.
+     */
+    private void filterExclusions( List<String> resourcePaths, List<String> patterns )
+    {
+        if ( patterns.isEmpty() )
+        {
+            // No exclusion patterns, nothing to filter
+            return;
+        }
+
+        for ( ListIterator<String> it = resourcePaths.listIterator(); it.hasNext(); )
+        {
+            String path = it.next();
+            if ( matchesAnyPattern( path, patterns ) )
+            {
+                it.remove();
+            }
+        }
+    }
+
+    /**
+     * Modifies the <code>resourcePaths</code> list removing all the paths not matched by the inclussion patterns.
+     * 
+     * If there are no inclusion patterns, the original list is not modified.
+     * 
+     * @param resourcePaths
+     *            the original resource paths for every modified resources in a resource folder.
+     * @param patterns
+     *            the inclusion patterns.
+     */
+    private void filterInclusions( List<String> resourcePaths, List<String> patterns )
+    {
+        if ( patterns.isEmpty() )
+        {
+            // No exclussion patterns, nothing to filter
+            return;
+        }
+
+        for ( ListIterator<String> it = resourcePaths.listIterator(); it.hasNext(); )
+        {
+            String path = it.next();
+            if ( !matchesAnyPattern( path, patterns ) )
+            {
+                it.remove();
+            }
+        }
+    }
+
+    /**
+     * Checks if the given path matches any of the specified patterns.
+     * 
+     * @param resourcePath
+     *            the path to check.
+     * @param patterns
+     *            the path patterns (ant style).
+     * @return <code>true</code> if the path is matched by at least one of the patterns.
+     */
+    private boolean matchesAnyPattern( String resourcePath, List<String> patterns )
+    {
+        for ( String pattern : patterns )
+        {
+            boolean matches = SelectorUtils.matchPath( pattern, resourcePath );
+            if ( matches )
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Gets the path of the affected files in any nesting level.
+     * 
+     * An affected file is any file contained in the delta. Folders and Projects are always ignored.
+     * 
+     * @param delta
+     *            the delta whose affected paths are being recovered.
+     * @return the affected paths, as <code>String</code>s.
+     */
+    private List<String> getAllPaths( IResourceDelta delta )
+    {
+        List<String> result = new LinkedList<String>();
+        Queue<IResourceDelta> pendingDeltas = new LinkedList<IResourceDelta>();
+        pendingDeltas.add( delta );
+        while ( !pendingDeltas.isEmpty() )
+        {
+            // get the first element in the queue
+            IResourceDelta currentDelta = pendingDeltas.remove();
+            if ( currentDelta.getResource().getType() == IResource.FILE )
+            {
+                // Only update on file changes
+                result.add( currentDelta.getResource().getProjectRelativePath().toString() );
+            }
+            pendingDeltas.addAll( Arrays.asList( currentDelta.getAffectedChildren() ) );
+        }
+        return result;
+    }
+
+    /**
+     * @param mavenProject
+     * @param resourceList
      * @return
      */
-    private boolean deltaContainsResource( IResourceDelta delta, List<Resource> resourceList )
+    private Map<Resource, IPath> getPathForResources( IMavenProject mavenProject, List<Resource> resourceList )
     {
-        IWorkspaceRoot workspaceRoot = getProject().getWorkspace().getRoot();
-        IProject project = getProject();
+        Map<Resource, IPath> result = new HashMap<Resource, IPath>( resourceList.size() );
+        IProject project = mavenProject.getProject();
+        IWorkspaceRoot workspaceRoot = project.getWorkspace().getRoot();
         for ( Resource r : resourceList )
         {
             String dirName = r.getDirectory();
@@ -151,19 +293,30 @@ public class MavenIncrementalBuilder
                         // Mapped to a resource in a different project
                         continue;
                     }
-                    IPath relative = c.getProjectRelativePath();
-                    MavenJdtCoreActivator.trace( TraceOption.MAVEN_INCREMENTAL_BUILDER, "Looking at: ", relative );
-                    if ( delta.findMember( relative ) != null )
-                    {
-                        // at least one resource is affected by this change.
-                        MavenJdtCoreActivator.trace( TraceOption.MAVEN_INCREMENTAL_BUILDER, "Found modification: ",
-                                                     relative );
-                        return true;
-                    }
+                    result.put( r, c.getProjectRelativePath() );
                 }
             }
+            else
+            {
+                // amuino: I haven't seen a case for this, but maven code seems to imply it is possible.
+                result.put( r, new Path( dirName ) );
+            }
         }
-        return false;
+        return result;
+    }
+
+    /**
+     * @param mavenProject
+     * @param goal
+     * @throws CoreException
+     */
+    private void onResourcesChange( IMavenProject mavenProject, String goal ) throws CoreException
+    {
+        MavenJdtCoreActivator.trace( TraceOption.MAVEN_INCREMENTAL_BUILDER, "Processing resources on ", getProject(),
+                                     " : ", goal );
+        MavenExecutionParameter params = MavenExecutionParameter.newDefaultMavenExecutionParameter();
+        params.setRecursive( false );
+        MavenManager.getMaven().scheduleGoal( mavenProject, goal, params );
     }
 
     private void onPomChange( IProject project, IProgressMonitor monitor )
@@ -175,8 +328,7 @@ public class MavenIncrementalBuilder
         {
             new IWorkspaceRunnable()
             {
-                public void run( IProgressMonitor monitor )
-                    throws CoreException
+                public void run( IProgressMonitor monitor ) throws CoreException
                 {
                     pom.deleteMarkers( MavenJdtCoreActivator.MARKER_ID, false, IResource.DEPTH_ZERO );
                 }
