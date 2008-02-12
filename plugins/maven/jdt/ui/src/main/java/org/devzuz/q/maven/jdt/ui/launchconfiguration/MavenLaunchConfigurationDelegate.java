90504ab3266dc333bed4fe8701bcd87d9c0602d2
emantos <emantos@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-02-12 16:28:45 +0000

    Small refactoring to make the launching of the maven event view usable in some other parts.

--- plugins/maven/jdt/ui/src/main/java/org/devzuz/q/maven/jdt/ui/launchconfiguration/MavenLaunchConfigurationDelegate.java
+++ plugins/maven/jdt/ui/src/main/java/org/devzuz/q/maven/jdt/ui/launchconfiguration/MavenLaunchConfigurationDelegate.java
@@ -14,17 +14,12 @@ import java.util.Properties;
 import org.devzuz.q.maven.embedder.IMavenProject;
 import org.devzuz.q.maven.embedder.MavenExecutionParameter;
 import org.devzuz.q.maven.embedder.MavenManager;
-import org.devzuz.q.maven.jdt.ui.MavenJdtUiActivator;
-import org.devzuz.q.maven.ui.views.MavenEventView;
+import org.devzuz.q.maven.jdt.ui.MavenJdtUiUtils;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
 
 public class MavenLaunchConfigurationDelegate implements ILaunchConfigurationDelegate
 {
@@ -52,102 +47,11 @@ public class MavenLaunchConfigurationDelegate implements ILaunchConfigurationDel
             {
                 if ( properties == null || properties.size() <= 0 )
                 {
-                    runMavenEventView();
+                    MavenJdtUiUtils.runMavenEventView();
                     MavenManager.getMaven().scheduleGoals( mavenProject, goals, 
                                                            MavenExecutionParameter.newDefaultMavenExecutionParameter( properties ) );
                 }
             }
-        }
-    }
-    
-    private void runMavenEventView()
-    {
-        IWorkbenchWindow ww = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-        IWorkbenchPage wp = null;
-        boolean isDisplayThread = true;
-        
-        // if no active workbench window, try to cycle thru all the workbenches to see 
-        // who can offer us an active page or if non exist, a page.
-        // This is important because if the main window is not on focus, there is no
-        // active workbench window.
-        if( ww == null )
-        {
-            isDisplayThread = false;
-            IWorkbenchWindow[] workbenches = PlatformUI.getWorkbench().getWorkbenchWindows();
-            for( IWorkbenchWindow workbench : workbenches )
-            {
-                IWorkbenchPage activePage = workbench.getActivePage();
-                if( activePage != null )
-                {
-                    ww = workbench;
-                    wp = activePage;
-                    break;
-                }
-            }
-            
-            if ( wp == null )
-            {
-                for ( IWorkbenchWindow workbench : workbenches )
-                {
-                    if ( workbench.getPages().length > 0 )
-                    {
-                        ww = workbench;
-                        wp = workbench.getPages()[0];
-                        break;
-                    }
-                }
-            }
-        }
-        else
-        {
-            wp = ww.getActivePage();
-            if( ( wp == null ) && ( ww.getPages().length > 0 ) )
-            {
-                wp = ww.getPages()[0];
-            }
-        }
-        
-        if( ww != null )
-        {
-            if( wp != null )
-            {
-                if( isDisplayThread )
-                {
-                    showView( wp , MavenEventView.VIEW_ID );
-                }
-                else
-                {
-                    final IWorkbenchPage page = wp;
-                    ww.getShell().getDisplay().asyncExec( new Runnable() 
-                    {
-                        public void run()
-                        {
-                            showView( page , MavenEventView.VIEW_ID );
-                        }
-                    });
-                }
-            }
-            else
-            {
-                MavenJdtUiActivator.getLogger().info( "Unable to open Maven Event View, IWorkbenchPage is null" );
-            }
-        }
-        else
-        {
-            MavenJdtUiActivator.getLogger().info( "Unable to open Maven Event View, IWorkbenchWindow is null" );
-        }
-    }
-
-    private void showView( IWorkbenchPage page , String viewId )
-    {
-        try
-        {
-            page.showView( viewId );
-        }
-        catch ( PartInitException e )
-        {
-            MavenJdtUiActivator.getLogger().log( "Unable to open Maven Event View", e );
-            return;
         }
     }
 }
