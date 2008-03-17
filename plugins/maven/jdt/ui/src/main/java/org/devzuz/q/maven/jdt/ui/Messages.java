d9dbb634c60f5201cd1153fba03137b2091fc537
emantos <emantos@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-03-17 04:13:10 +0000

    Removed unused messages in jdt.ui message properties. Added copyright header.

--- plugins/maven/jdt/ui/src/main/java/org/devzuz/q/maven/jdt/ui/Messages.java
+++ plugins/maven/jdt/ui/src/main/java/org/devzuz/q/maven/jdt/ui/Messages.java
@@ -13,58 +13,6 @@ import org.eclipse.osgi.util.NLS;
 public class Messages
 {
     private static final String BUNDLE_NAME = MavenJdtUiActivator.PLUGIN_ID + ".messages"; //$NON-NLS-1$
-
-    public static String MavenPomEditor_MavenPomEditor_BasicInformation;
-
-    public static String MavenPomEditor_MavenPomEditor_Links;
-
-    public static String MavenPomEditor_MavenPomEditor_ParentPOM;
-
-    public static String MavenPomEditor_MavenPomEditor_MoreProjInfo;
-
-    public static String MavenPomEditor_MavenPomEditor_Properties;
-
-    public static String MavenPomEditor_MavenPomEditor_GroupId;
-
-    public static String MavenPomEditor_MavenPomEditor_ArtifactId;
-
-    public static String MavenPomEditor_MavenPomEditor_Version;
-
-    public static String MavenPomEditor_MavenPomEditor_Type;
-
-    public static String MavenPomEditor_MavenPomEditor_Scope;
-
-    public static String MavenPomEditor_MavenPomEditor_SystemPath;
-
-    public static String MavenPomEditor_MavenPomEditor_Optional;
-
-    public static String MavenPomEditor_MavenPomEditor_Packaging;
-
-    public static String MavenPomEditor_MavenPomEditor_Classifier;
-
-    public static String MavenPomEditor_MavenPomEditor_RelativePath;
-
-    public static String MavenPomEditor_MavenPomEditor_AddButton;
-
-    public static String MavenPomEditor_MavenPomEditor_EditButton;
-
-    public static String MavenPomEditor_MavenPomEditor_RemoveButton;
-
-    public static String MavenPomEditor_MavenPomEditor_Key;
-
-    public static String MavenPomEditor_MavenPomEditor_Value;
-
-    public static String MavenPomEditor_MavenPomEditor_Modules;
-
-    public static String MavenPomEditor_MavenPomEditor_Module;
-
-    public static String MavenPomEditor_MavenPomEditor_Name;
-
-    public static String MavenPomEditor_MavenPomEditor_Description;
-
-    public static String MavenPomEditor_MavenPomEditor_URL;
-
-    public static String MavenPomEditor_MavenPomEditor_InceptionYear;
     
     public static String MavenLaunchShortcut_NoLaunchConfigFound;
     
