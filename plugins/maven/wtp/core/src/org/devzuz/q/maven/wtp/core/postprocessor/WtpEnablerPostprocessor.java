92799a997710a3de3dc386754cce14196b453bd7
amuino <amuino@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-04-26 18:58:38 +0000

    [329]: WTP integration does not deploy module references to webapp http://code.google.com/p/q4e/issues/detail?id=329
    Automatically set up J2EE dependencies to let WTP deploy references in the workspace.

--- plugins/maven/wtp/core/src/org/devzuz/q/maven/wtp/core/postprocessor/WtpEnablerPostprocessor.java
+++ plugins/maven/wtp/core/src/org/devzuz/q/maven/wtp/core/postprocessor/WtpEnablerPostprocessor.java
@@ -16,18 +16,26 @@ import org.apache.maven.artifact.Artifact;
 import org.apache.maven.model.Build;
 import org.devzuz.q.maven.embedder.IMavenArtifact;
 import org.devzuz.q.maven.embedder.IMavenProject;
+import org.devzuz.q.maven.embedder.MavenManager;
 import org.devzuz.q.maven.jdt.core.classpath.container.IMavenClasspathAttributeProvider;
 import org.devzuz.q.maven.jdt.ui.projectimport.IImportProjectPostprocessor;
 import org.devzuz.q.maven.wtp.core.MavenWtpActivator;
 import org.devzuz.q.maven.wtp.core.internal.TraceOption;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IClasspathAttribute;
+import org.eclipse.jem.util.emf.workbench.ProjectUtilities;
 import org.eclipse.jst.j2ee.classpathdep.UpdateClasspathAttributeUtil;
 import org.eclipse.jst.j2ee.web.project.facet.IWebFacetInstallDataModelProperties;
+import org.eclipse.wst.common.componentcore.ComponentCore;
+import org.eclipse.wst.common.componentcore.internal.operation.CreateReferenceComponentsOp;
+import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
+import org.eclipse.wst.common.componentcore.resources.IVirtualReference;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
+import org.eclipse.wst.common.project.facet.core.FacetedProjectFramework;
 import org.eclipse.wst.common.project.facet.core.IActionDefinition;
 import org.eclipse.wst.common.project.facet.core.IFacetedProject;
 import org.eclipse.wst.common.project.facet.core.IProjectFacet;
@@ -61,7 +69,7 @@ public class WtpEnablerPostprocessor implements IImportProjectPostprocessor, IMa
         try
         {
             MavenWtpActivator.trace( TraceOption.POSTPROCESSOR, "Adding WTP facets to ", mavenProject );
-            addProjectFacets( mavenProject, monitor );
+            addWebProjectFacets( mavenProject, monitor );
         }
         catch ( CoreException e )
         {
@@ -81,7 +89,6 @@ public class WtpEnablerPostprocessor implements IImportProjectPostprocessor, IMa
     {
         if ( isWarProject( mavenProject ) && !isDeployable( artifact ) )
         {
-
             try
             {
                 MavenWtpActivator.trace( TraceOption.CP_ATTRIBUTE_PROVIDER, "Adding non-dependency attribute to ",
@@ -96,7 +103,91 @@ public class WtpEnablerPostprocessor implements IImportProjectPostprocessor, IMa
                                                                    + " for maven project " + mavenProject, e );
             }
         }
+        if ( isWarProject( mavenProject ) && isDeployable( artifact ) && isInWorkspace )
+        {
+            addAsJEEDependency( mavenProject, artifact );
+        }
         return Collections.emptySet();
+    }
+
+    /**
+     * Registers the artifact as a JEE reference. Must be an artifact referenced from the workspace.
+     * 
+     * @param mavenProject
+     * @param artifact
+     */
+    private void addAsJEEDependency( IMavenProject mavenProject, IMavenArtifact artifact )
+    {
+        IProject project =
+            MavenManager.getMavenProjectManager().getWorkspaceProject( artifact.getGroupId(), artifact.getArtifactId(),
+                                                                       artifact.getVersion() );
+        IVirtualComponent affected = ComponentCore.createComponent( mavenProject.getProject() );
+        try
+        {
+            IMavenProject mavenDependencyProject =
+                MavenManager.getMavenProjectManager().getMavenProject( project, false );
+            // Must be an utility project to add as a JEE reference
+            addUtilityProjectFacets( mavenDependencyProject, null );
+            IVirtualComponent referenced = ComponentCore.createComponent( project );
+            IPath deployPath = new Path( "/WEB-INF/lib" );
+            // Check if already referenced
+            if ( !srcComponentContainsReference( affected, referenced, deployPath ) )
+            {
+                IVirtualReference reference = ComponentCore.createReference( affected, referenced );
+                reference.setRuntimePath( deployPath );
+                reference.setDependencyType( IVirtualReference.DEPENDENCY_TYPE_USES );
+                affected.addReferences( new IVirtualReference[] { reference } );
+                ProjectUtilities.addReferenceProjects( mavenProject.getProject(), project );
+            }
+        }
+        catch ( CoreException e )
+        {
+            MavenWtpActivator.getLogger().log( "Unable to add JEE project references", e );
+        }
+    }
+
+    /**
+     * Checks if a dependency is already present on a component.
+     * 
+     * XXX Copied from {@link CreateReferenceComponentsOp}
+     * 
+     * @param sourceComp
+     *            the component.
+     * @param comp
+     *            the dependency to check.
+     * @param deployPath
+     *            use this deploy path when comparing (dependencies in different deploy paths will not match).
+     * @return <code>true</code> if the source component contains a reference to the target component.
+     */
+    private boolean srcComponentContainsReference( IVirtualComponent sourceComp, IVirtualComponent comp,
+                                                   IPath deployPath )
+    {
+        if ( ( sourceComp != null && sourceComp.getProject() != null ) && ( comp != null && comp.getProject() != null ) )
+        {
+            IVirtualReference[] existingReferences = sourceComp.getReferences();
+            IVirtualComponent referencedComponent = null;
+            if ( existingReferences != null )
+            {
+                for ( int i = 0; i < existingReferences.length; i++ )
+                {
+                    IVirtualReference ref = existingReferences[i];
+                    if ( ref != null )
+                    {
+                        // also check to see if the deploy path is the same (remember that it can be null)
+                        if ( ( ref.getRuntimePath() == null && deployPath != null )
+                                        || ref.getRuntimePath().equals( deployPath ) )
+                        {
+                            referencedComponent = ref.getReferencedComponent();
+                            if ( referencedComponent != null && referencedComponent.equals( comp ) )
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     /**
@@ -132,35 +223,86 @@ public class WtpEnablerPostprocessor implements IImportProjectPostprocessor, IMa
     // Utility methods
     //
 
-    private void addProjectFacets( IMavenProject mavenProject, IProgressMonitor monitor ) throws CoreException
+    protected void addWebProjectFacets( IMavenProject mavenProject, IProgressMonitor monitor ) throws CoreException
     {
         IFacetedProject facetedProject = ProjectFacetsManager.create( mavenProject.getProject(), true, null );
-        IProjectFacet webFacet = ProjectFacetsManager.getProjectFacet( "jst.web" );
-        IProjectFacet javaFacet = ProjectFacetsManager.getProjectFacet( "jst.java" );
-        IProjectFacetVersion javaFacetVersion = javaFacet.getVersion( "5.0" );
-        IProjectFacetVersion webFacetVersion = webFacet.getVersion( "2.4" );
-        Set<Action> facetActions = new HashSet<Action>( 2 );
-        Build buildModel = mavenProject.getModel().getBuild();
-        for ( IActionDefinition def : javaFacetVersion.getActionDefinitions( Action.Type.INSTALL ) )
-        {
-            IDataModel model =
-                (IDataModel) def.createConfigObject( javaFacetVersion, mavenProject.getProject().getName() );
-            model.setStringProperty( "IJavaFacetInstallDataModelProperties.SOURCE_FOLDER_NAME",
-                                     makeRelative( mavenProject, buildModel.getSourceDirectory() ) );
-            model.setStringProperty( "IJavaFacetInstallDataModelProperties.DEFAULT_OUTPUT_FOLDER_NAME",
-                                     makeRelative( mavenProject, buildModel.getOutputDirectory() ) );
-            facetActions.add( new Action( Action.Type.INSTALL, javaFacetVersion, null ) );
-        }
-        for ( IActionDefinition def : webFacetVersion.getActionDefinitions( Action.Type.INSTALL ) )
-        {
-            IDataModel model =
-                (IDataModel) def.createConfigObject( webFacetVersion, mavenProject.getProject().getName() );
-            model.setStringProperty( IWebFacetInstallDataModelProperties.SOURCE_FOLDER, "src/main/java" );
-            model.setStringProperty( IWebFacetInstallDataModelProperties.CONFIG_FOLDER, "src/main/webapp" );
-            model.setStringProperty( IWebFacetInstallDataModelProperties.CONTEXT_ROOT, mavenProject.getArtifactId() );
-            facetActions.add( new Action( Action.Type.INSTALL, webFacetVersion, model ) );
-        }
+        Set<Action> facetActions = new HashSet<Action>();
+        facetActions.addAll( getJavaFacetInstallActions( mavenProject, monitor ) );
+        facetActions.addAll( getWebFacetInstallActions( mavenProject, monitor ) );
         facetedProject.modify( facetActions, monitor );
+    }
+
+    protected void addUtilityProjectFacets( IMavenProject mavenProject, IProgressMonitor monitor ) throws CoreException
+    {
+        IFacetedProject facetedProject = ProjectFacetsManager.create( mavenProject.getProject(), true, null );
+        Set<Action> facetActions = new HashSet<Action>();
+        facetActions.addAll( getJavaFacetInstallActions( mavenProject, monitor ) );
+        facetActions.addAll( getUtilityFacetInstallActions( mavenProject, monitor ) );
+        facetedProject.modify( facetActions, monitor );
+    }
+
+    protected Set<Action> getJavaFacetInstallActions( IMavenProject mavenProject, IProgressMonitor monitor )
+        throws CoreException
+    {
+        Set<Action> facetActions = new HashSet<Action>();
+        if ( !FacetedProjectFramework.hasProjectFacet( mavenProject.getProject(), "jst.java" ) )
+        {
+            IProjectFacet javaFacet = ProjectFacetsManager.getProjectFacet( "jst.java" );
+            // TODO: Extract from the maven compiler plugin configuration
+            IProjectFacetVersion javaFacetVersion = javaFacet.getVersion( "5.0" );
+            Build buildModel = mavenProject.getModel().getBuild();
+            for ( IActionDefinition def : javaFacetVersion.getActionDefinitions( Action.Type.INSTALL ) )
+            {
+                IDataModel model =
+                    (IDataModel) def.createConfigObject( javaFacetVersion, mavenProject.getProject().getName() );
+                model.setStringProperty( "IJavaFacetInstallDataModelProperties.SOURCE_FOLDER_NAME",
+                                         makeRelative( mavenProject, buildModel.getSourceDirectory() ) );
+                model.setStringProperty( "IJavaFacetInstallDataModelProperties.DEFAULT_OUTPUT_FOLDER_NAME",
+                                         makeRelative( mavenProject, buildModel.getOutputDirectory() ) );
+                facetActions.add( new Action( Action.Type.INSTALL, javaFacetVersion, null ) );
+            }
+        }
+        return facetActions;
+    }
+
+    protected Set<Action> getWebFacetInstallActions( IMavenProject mavenProject, IProgressMonitor monitor )
+        throws CoreException
+    {
+        Set<Action> facetActions = new HashSet<Action>();
+        if ( !FacetedProjectFramework.hasProjectFacet( mavenProject.getProject(), "jst.web" ) )
+        {
+            IProjectFacet webFacet = ProjectFacetsManager.getProjectFacet( "jst.web" );
+            // TODO: Extract from the version of jsp/servlet dependencies
+            IProjectFacetVersion webFacetVersion = webFacet.getVersion( "2.4" );
+            for ( IActionDefinition def : webFacetVersion.getActionDefinitions( Action.Type.INSTALL ) )
+            {
+                IDataModel model =
+                    (IDataModel) def.createConfigObject( webFacetVersion, mavenProject.getProject().getName() );
+                model.setStringProperty( IWebFacetInstallDataModelProperties.SOURCE_FOLDER, "src/main/java" );
+                model.setStringProperty( IWebFacetInstallDataModelProperties.CONFIG_FOLDER, "src/main/webapp" );
+                model.setStringProperty( IWebFacetInstallDataModelProperties.CONTEXT_ROOT, mavenProject.getArtifactId() );
+                facetActions.add( new Action( Action.Type.INSTALL, webFacetVersion, model ) );
+            }
+        }
+        return facetActions;
+    }
+
+    protected Set<Action> getUtilityFacetInstallActions( IMavenProject mavenProject, IProgressMonitor monitor )
+        throws CoreException
+    {
+        Set<Action> facetActions = new HashSet<Action>();
+        if ( !FacetedProjectFramework.hasProjectFacet( mavenProject.getProject(), "jst.utility" ) )
+        {
+            IProjectFacet utilityFacet = ProjectFacetsManager.getProjectFacet( "jst.utility" );
+            IProjectFacetVersion utilityFacetVersion = utilityFacet.getDefaultVersion();
+            for ( IActionDefinition def : utilityFacetVersion.getActionDefinitions( Action.Type.INSTALL ) )
+            {
+                IDataModel model =
+                    (IDataModel) def.createConfigObject( utilityFacetVersion, mavenProject.getProject().getName() );
+                facetActions.add( new Action( Action.Type.INSTALL, utilityFacetVersion, model ) );
+            }
+        }
+        return facetActions;
     }
 
     /**
