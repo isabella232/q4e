06ad50febcdd1497f6afe8e26ea37f1e87e083b4
amuino <amuino@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-05-07 21:25:37 +0000

    Package maven plug-ins in the workspace for use during the build.

--- plugins/maven/core/src/org/devzuz/q/maven/embedder/internal/EclipseMavenRequest.java
+++ plugins/maven/core/src/org/devzuz/q/maven/embedder/internal/EclipseMavenRequest.java
@@ -24,7 +24,9 @@ import org.devzuz.q.maven.embedder.IMavenProject;
 import org.devzuz.q.maven.embedder.MavenCoreActivator;
 import org.devzuz.q.maven.embedder.MavenExecutionStatus;
 import org.devzuz.q.maven.embedder.MavenInterruptedException;
+import org.devzuz.q.maven.embedder.MavenManager;
 import org.devzuz.q.maven.embedder.MavenMonitorHolder;
+import org.devzuz.q.maven.embedder.MavenProjectManager;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
@@ -42,6 +44,16 @@ import org.eclipse.core.runtime.jobs.Job;
  */
 public class EclipseMavenRequest extends Job implements IMavenJob
 {
+
+    /**
+     * Each maven plug-in project in the workspace consumes this much ticks in the progress monitor while being packaged
+     */
+    private static final int TICKS_FOR_PACKAGING_PLUGINS = 1;
+
+    /**
+     * Each dependency project in the workspace consumes this much ticks in the progress monitor while being packaged
+     */
+    private static final int TICKS_FOR_PACKAGING_DEPENDENCIES = 10;
 
     private final EclipseMaven maven;
 
@@ -73,12 +85,14 @@ public class EclipseMavenRequest extends Job implements IMavenJob
         List<MojoBinding> mojos = getExecutionMojos();
         if ( mojos != null )
         {
-            totalWork = mojos.size() * 2; // every mojo triggers 2 events: start and end
+            // every mojo triggers 2 events: start and end.
+            // Allow extra ticks for packaging the mojo if it is on the workspace.
+            totalWork = mojos.size() * ( 2 + TICKS_FOR_PACKAGING_PLUGINS );
         }
         // allow 10 ticks for packaging each dependency
         if ( mavenProject != null )
         {
-            totalWork += mavenProject.getDependencyProjects().size() * 10;
+            totalWork += mavenProject.getDependencyProjects().size() * TICKS_FOR_PACKAGING_DEPENDENCIES;
             monitor.beginTask( mavenProject.toString() + ": " + request.getGoals(), totalWork );
         }
         else
@@ -156,6 +170,24 @@ public class EclipseMavenRequest extends Job implements IMavenJob
         {
             return;
         }
+        // Package plug-ins in the workspace
+        MavenProjectManager mavenProjectManager = MavenManager.getMavenProjectManager();
+        for ( MojoBinding mojo : mojos )
+        {
+            try
+            {
+                IMavenProject mojoProject =
+                    mavenProjectManager.getMavenProject( mojo.getGroupId(), mojo.getArtifactId(), mojo.getVersion(),
+                                                         false );
+                packageIfNeeded( mojoProject, monitor, TICKS_FOR_PACKAGING_PLUGINS );
+            }
+            catch ( CoreException e )
+            {
+                MavenCoreActivator.getLogger().log(
+                                                    "Error retrieving maven project on the workspace  for mojo: "
+                                                                    + mojo, e );
+            }
+        }
         // Direct dependencies
         Set<IMavenProject> projects = mavenProject.getDependencyProjects();
         if ( projects.isEmpty() || !isPackagingRequired( mojos ) )
@@ -165,43 +197,100 @@ public class EclipseMavenRequest extends Job implements IMavenJob
         for ( IMavenProject dependencyProject : projects )
         {
             monitor.subTask( "Packaging dependency: " + dependencyProject.toString() );
-            if ( isRepackagingRequired( dependencyProject ) )
+            packageIfNeeded( dependencyProject, monitor, TICKS_FOR_PACKAGING_DEPENDENCIES );
+        }
+    }
+
+    /**
+     * Package the given maven project if it is on the workspace and has been modified since the last time it was
+     * packaged.
+     * 
+     * @param dependencyProject
+     *            the project to package. Migth be <code>null</code> in which case packaging will be skiped
+     * @param monitor
+     *            the monitor used for progress reporting
+     * @param ticksUsed
+     *            the number of ticks consumed from the progress monitor
+     */
+    private void packageIfNeeded( IMavenProject dependencyProject, IProgressMonitor monitor, int ticksUsed )
+    {
+        if ( null != dependencyProject && isRepackagingRequired( dependencyProject ) )
+        {
+            try
             {
+                maven.executeGoal( dependencyProject, "package", new SubProgressMonitor( monitor, ticksUsed ) );
+                // Make sure that the file timestamp is updated.
+                IFile file = maven.getGeneratedArtifactFile( dependencyProject );
                 try
                 {
-                    maven.executeGoal( dependencyProject, "package", new SubProgressMonitor( monitor, 10 ) );
-                    // Make sure that the file timestamp is updated.
-                    IFile file = maven.getGeneratedArtifactFile( dependencyProject );
-                    try
+                    IProject eclipseProject = dependencyProject.getProject();
+                    // If the project was no timestamp info, set to "now" (this will happen on an upgrade or
+                    // import).
+                    if ( null == eclipseProject.getPersistentProperty( IMavenProject.CHANGE_TIMESTAMP ) )
                     {
-                        IProject eclipseProject = dependencyProject.getProject();
-                        // If the project was no timestamp info, set to "now" (this will happen on an upgrade or
-                        // import).
-                        if ( null == eclipseProject.getPersistentProperty( IMavenProject.CHANGE_TIMESTAMP ) )
-                        {
-                            // Make the buildtime 1 second in the past, since some OS don't keep milliseconds on the
-                            // file dates
-                            eclipseProject.setPersistentProperty( IMavenProject.CHANGE_TIMESTAMP,
-                                                                  String.valueOf( System.currentTimeMillis() - 1000 ) );
-                        }
-                        file.setLocalTimeStamp( System.currentTimeMillis() );
+                        // Make the buildtime 1 second in the past, since some OS don't keep milliseconds on the
+                        // file dates
+                        eclipseProject.setPersistentProperty( IMavenProject.CHANGE_TIMESTAMP,
+                                                              String.valueOf( System.currentTimeMillis() - 1000 ) );
                     }
-                    catch ( CoreException e )
-                    {
-                        MavenCoreActivator.getLogger().warn( "Could not update modification date for " + file, e );
-
-                    }
+                    file.setLocalTimeStamp( System.currentTimeMillis() );
                 }
                 catch ( CoreException e )
                 {
-                    MavenCoreActivator.getLogger().log( "Error packaging dependency project: " + dependencyProject, e );
+                    MavenCoreActivator.getLogger().warn( "Could not update modification date for " + file, e );
+
                 }
             }
-            else
+            catch ( CoreException e )
             {
-                monitor.worked( 10 );
+                MavenCoreActivator.getLogger().log( "Error packaging dependency project: " + dependencyProject, e );
             }
         }
+        else
+        {
+            monitor.worked( ticksUsed );
+        }
+    }
+
+    /**
+     * Runs the package goal in the project and updates its change timestamp if needed.
+     * 
+     * @param dependencyProject
+     *            the project on the workspace
+     * @param monitor
+     *            used for reporting progress
+     * @param numTicks
+     *            number of ticks to be consumed from the monitor
+     * @return the {@link IFile} corresponding to the built artifact
+     * @throws CoreException
+     *             if the package goal can not be executed successfully
+     */
+    private IFile packageWorkspaceProject( IMavenProject dependencyProject, IProgressMonitor monitor, int numTicks )
+        throws CoreException
+    {
+        maven.executeGoal( dependencyProject, "package", new SubProgressMonitor( monitor, numTicks ) );
+        // Make sure that the file timestamp is updated.
+        IFile file = maven.getGeneratedArtifactFile( dependencyProject );
+        try
+        {
+            IProject eclipseProject = dependencyProject.getProject();
+            // If the project was no timestamp info, set to "now" (this will happen on an upgrade or
+            // import).
+            if ( null == eclipseProject.getPersistentProperty( IMavenProject.CHANGE_TIMESTAMP ) )
+            {
+                // Make the buildtime 1 second in the past, since some OS don't keep milliseconds on the
+                // file dates
+                eclipseProject.setPersistentProperty( IMavenProject.CHANGE_TIMESTAMP,
+                                                      String.valueOf( System.currentTimeMillis() - 1000 ) );
+            }
+            file.setLocalTimeStamp( System.currentTimeMillis() );
+        }
+        catch ( CoreException e )
+        {
+            MavenCoreActivator.getLogger().warn( "Could not update modification date for " + file, e );
+
+        }
+        return file;
     }
 
     /**
