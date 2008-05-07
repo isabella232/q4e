e2f9dd6ec880817db8321d70150f8e2301ac3183
amuino <amuino@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-05-07 21:29:36 +0000

    Delegation on the DefaultProjectBuilder caused a NPE in the embedder on some cases, so MavenProject resolution is short circuited for projects in the workspace (avoid going all the way down to the embedder).

--- plugins/maven/core/src/org/devzuz/q/maven/embedder/internal/EclipseMavenProjectBuilder.java
+++ plugins/maven/core/src/org/devzuz/q/maven/embedder/internal/EclipseMavenProjectBuilder.java
@@ -7,47 +7,17 @@
 package org.devzuz.q.maven.embedder.internal;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.Writer;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
 
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.DependencyResolutionRequiredException;
-import org.apache.maven.artifact.factory.ArtifactFactory;
 import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.resolver.filter.ArtifactFilter;
-import org.apache.maven.model.Build;
-import org.apache.maven.model.CiManagement;
-import org.apache.maven.model.Contributor;
-import org.apache.maven.model.DependencyManagement;
-import org.apache.maven.model.Developer;
-import org.apache.maven.model.DistributionManagement;
-import org.apache.maven.model.IssueManagement;
-import org.apache.maven.model.License;
-import org.apache.maven.model.MailingList;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.Organization;
-import org.apache.maven.model.Plugin;
-import org.apache.maven.model.PluginManagement;
-import org.apache.maven.model.Prerequisites;
-import org.apache.maven.model.Reporting;
-import org.apache.maven.model.Resource;
-import org.apache.maven.model.Scm;
 import org.apache.maven.profiles.ProfileManager;
 import org.apache.maven.project.DefaultMavenProjectBuilder;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.ProjectBuildingException;
-import org.apache.maven.project.artifact.InvalidDependencyVersionException;
-import org.codehaus.plexus.util.xml.Xpp3Dom;
 import org.devzuz.q.maven.embedder.IMavenProject;
 import org.devzuz.q.maven.embedder.MavenCoreActivator;
 import org.devzuz.q.maven.embedder.MavenManager;
-import org.devzuz.q.maven.embedder.MavenProjectManager;
 import org.eclipse.core.runtime.CoreException;
 
 /**
@@ -74,16 +44,55 @@ public class EclipseMavenProjectBuilder extends DefaultMavenProjectBuilder
                                              ArtifactRepository localRepository, boolean allowStub )
         throws ProjectBuildingException
     {
-        return new MavenProjectWrapper( super.buildFromRepository( artifact, remoteArtifactRepositories,
-                                                                   localRepository, allowStub ) );
+        IMavenProject mavenProject = getMavenProjectFromWorkspace( artifact );
+        if ( mavenProject != null )
+        {
+            return mavenProject.getRawMavenProject();
+        }
+        else
+        {
+            return new MavenProjectWrapper( super.buildFromRepository( artifact, remoteArtifactRepositories,
+                                                                       localRepository, allowStub ) );
+        }
     }
 
     @Override
     public MavenProject buildFromRepository( Artifact artifact, List remoteArtifactRepositories,
                                              ArtifactRepository localRepository ) throws ProjectBuildingException
     {
-        return new MavenProjectWrapper( super.buildFromRepository( artifact, remoteArtifactRepositories,
-                                                                   localRepository ) );
+        IMavenProject mavenProject = getMavenProjectFromWorkspace( artifact );
+        if ( mavenProject != null )
+        {
+            return mavenProject.getRawMavenProject();
+        }
+        else
+        {
+            return new MavenProjectWrapper( super.buildFromRepository( artifact, remoteArtifactRepositories,
+                                                                       localRepository ) );
+        }
+    }
+
+    /**
+     * Gets the maven project from the workspace, logging any exceptions as warnings (since we can recover by delegating
+     * on the default behavior).
+     * 
+     * @param artifact
+     *            the artifact to resolve.
+     * @return the resolved maven project, or <code>null</code>
+     */
+    private IMavenProject getMavenProjectFromWorkspace( Artifact artifact )
+    {
+        try
+        {
+            return MavenManager.getMavenProjectManager().getMavenProject( artifact, false );
+        }
+        catch ( CoreException e )
+        {
+            MavenCoreActivator.getLogger().warn(
+                                                 "Could not build maven project from the workspace for artifact "
+                                                                 + artifact + ", using normal process", e );
+        }
+        return null;
     }
 
     @Override
