a6bcc71a00c71701f3f88b0744962b65767c6bc2
amuino <amuino@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-05-15 13:22:32 +0000

    Remote try-catch from unit tests, we want them to fail on an exception (that should not be raised).

--- plugins/maven/ui/src/test/java/org/devzuz/q/maven/ui/archetype/provider/impl/Archetype2CatalogProviderTest.java
+++ plugins/maven/ui/src/test/java/org/devzuz/q/maven/ui/archetype/provider/impl/Archetype2CatalogProviderTest.java
@@ -5,69 +5,56 @@ import java.util.Collection;
 
 import junit.framework.TestCase;
 
-import org.devzuz.q.maven.embedder.QCoreException;
 import org.devzuz.q.maven.ui.archetype.provider.Archetype;
 
-public class Archetype2CatalogProviderTest
-    extends TestCase
+public class Archetype2CatalogProviderTest extends TestCase
 {
-    public void testArchetype2CatalogProviderLegal()
+    public void testArchetype2CatalogProviderLegal() throws Exception
     {
-        URL url = getClass().getResource("Catalog.xml");
-        
+        URL url = getClass().getResource( "Catalog.xml" );
+
         Archetype2CatalogProvider provider = new Archetype2CatalogProviderMock();
-        
+
         provider.setCatalogFilename( url.getFile() );
-        
-        try
-        {
-            Collection< Archetype > archetypes = provider.getArchetypes();
-            assertTrue( archetypes.size() == 1 );
-            
-            Archetype archetype = (Archetype) archetypes.toArray()[0];
-            assertTrue( archetype.getGroupId().equals( "org.appfuse.archetypes" ) );
-            assertTrue( archetype.getArtifactId().equals( "appfuse-basic-jsf" ) );
-            assertTrue( archetype.getVersion().equals( "2.0" ) );
-            assertTrue( archetype.getRemoteRepositories().equals( "http://static.appfuse.org/releases" ) );
-            assertTrue( archetype.getDescription().equals( "AppFuse archetype for creating a web application with Hibernate, Spring and JSF" ) );
-        }
-        catch ( QCoreException e )
-        {
-            e.printStackTrace();
-        }
+
+        Collection<Archetype> archetypes = provider.getArchetypes();
+        assertTrue( archetypes.size() == 1 );
+
+        Archetype archetype = (Archetype) archetypes.toArray()[0];
+        assertTrue( archetype.getGroupId().equals( "org.appfuse.archetypes" ) );
+        assertTrue( archetype.getArtifactId().equals( "appfuse-basic-jsf" ) );
+        assertTrue( archetype.getVersion().equals( "2.0" ) );
+        assertTrue( archetype.getRemoteRepositories().equals( "http://static.appfuse.org/releases" ) );
+        assertTrue( archetype.getDescription().equals(
+                                                       "AppFuse archetype for creating a web application with Hibernate, Spring and JSF" ) );
     }
-    
-    public void testArchetype2CatalogProviderIllegal()
+
+    public void testArchetype2CatalogProviderIllegal() throws Exception
     {
-        URL url = getClass().getResource("Catalog2.xml");
-        
+        URL url = getClass().getResource( "Catalog2.xml" );
+
         Archetype2CatalogProvider provider = new Archetype2CatalogProviderMock();
-        
+
         provider.setCatalogFilename( url.getFile() );
-        
-        try
-        {
-            Collection< Archetype > archetypes = provider.getArchetypes();
-            assertTrue( archetypes.size() == 2 );
-            
-            Archetype archetype = (Archetype) archetypes.toArray()[0];
-            assertTrue( archetype.getGroupId().equals( "org.appfuse.archetypes" ) );
-            assertTrue( archetype.getArtifactId().equals( "appfuse-basic-jsf" ) );
-            assertTrue( archetype.getVersion().equals( "2.0" ) );
-            assertTrue( archetype.getRemoteRepositories().equals( "http://static.appfuse.org/releases" ) );
-            assertTrue( archetype.getDescription().equals( "AppFuse archetype for creating a web application with Hibernate, Spring and JSF" ) );
-        }
-        catch ( QCoreException e )
-        {
-            e.printStackTrace();
-        }
+
+        Collection<Archetype> archetypes = provider.getArchetypes();
+        assertTrue( archetypes.size() == 2 );
+
+        Archetype archetype = (Archetype) archetypes.toArray()[0];
+        assertTrue( archetype.getGroupId().equals( "org.appfuse.archetypes" ) );
+        assertTrue( archetype.getArtifactId().equals( "appfuse-basic-jsf" ) );
+        assertTrue( archetype.getVersion().equals( "2.0" ) );
+        assertTrue( archetype.getRemoteRepositories().equals( "http://static.appfuse.org/releases" ) );
+        assertTrue( archetype.getDescription().equals(
+                                                       "AppFuse archetype for creating a web application with Hibernate, Spring and JSF" ) );
     }
-    
+
     class Archetype2CatalogProviderMock extends Archetype2CatalogProvider
     {
+        @Override
         protected void logError( String message )
         {
-            System.out.println("ERROR : " +  message );
+            System.out.println( "ERROR : " + message );
         }
     }
 }
