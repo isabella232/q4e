4c78f8459b846cff38f36171b15f0deda21b8051
ramirezag <ramirezag@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-06-02 06:57:18 +0000

    Fix for issue 457
    added enablement for MavenManageDependenciesAction objectContribution. It will now be shown only if the project is open and if it has pom.xml

--- plugins/maven/ui/src/main/java/org/devzuz/q/maven/ui/propertytester/MavenProjectPropertyTester.java
+++ plugins/maven/ui/src/main/java/org/devzuz/q/maven/ui/propertytester/MavenProjectPropertyTester.java
@@ -5,23 +5,21 @@ import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.core.resources.IProject;
 
 /**
- * This class adds support for testing properties specific to the q4e plug-in.
- * 
- * Supported properties are:
+ * This class adds support for testing properties specific to the q4e plug-in. Supported properties are:
  * <ul>
  * <li>hasPOM (true/false): checks if the project has a pom.xml file on the root.</li>
  * </ul>
  */
-public class MavenProjectPropertyTester extends PropertyTester
+public class MavenProjectPropertyTester
+    extends PropertyTester
 {
-
     public boolean test( Object receiver, String property, Object[] args, Object expectedValue )
     {
         if ( "hasPOM".equals( property ) )
         {
             IProject project = MavenUiUtil.adaptAs( IProject.class, receiver );
             boolean expected = ( (Boolean) expectedValue ).booleanValue();
-            if ( project != null )
+            if ( project != null && project.isOpen() )
             {
                 return project.getFile( "pom.xml" ).exists() == expected;
             }
