1b3d4a4eafe8f950f2048055372d0c9c4362bb27
amuino <amuino@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-06-08 13:48:39 +0000

    Remove TODO annotation referencing Issue 181 (already fixed).

--- plugins/maven/jdt/core/src/main/java/org/devzuz/q/maven/jdt/core/nature/MavenNature.java
+++ plugins/maven/jdt/core/src/main/java/org/devzuz/q/maven/jdt/core/nature/MavenNature.java
@@ -371,11 +371,6 @@ public class MavenNature implements IProjectNature
                 // Avoid duplicates
                 classpathSrcEntries.add( classpathEntry );
             }
-            else
-            {
-                // TODO Fix Issue 181
-                // combine the inclusions/exclusions with the previous classpath entry for that folder
-            }
         }
     }
 
