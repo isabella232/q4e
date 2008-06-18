2a88ff9f78bf55487bb8bf0a1392631808d99c6a
amuino <amuino@618d13e9-b832-0410-9acc-c1012e8a5ee6>
2008-06-18 00:18:47 +0000

    Use QUtil for adapting objects.

--- plugins/maven/ui/src/main/java/org/devzuz/q/maven/ui/internal/util/MavenUiUtil.java
+++ plugins/maven/ui/src/main/java/org/devzuz/q/maven/ui/internal/util/MavenUiUtil.java
@@ -1,5 +1,6 @@
 package org.devzuz.q.maven.ui.internal.util;
 
+import org.devzuz.q.util.QUtil;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IAdaptable;
@@ -33,54 +34,13 @@ public class MavenUiUtil
             IStructuredSelection structuredSelection = (IStructuredSelection) selection;
             Object object = structuredSelection.getFirstElement();
 
-            IResource asResource = adaptAs( IResource.class, object );
+            IResource asResource = QUtil.adaptAs( IResource.class, object );
             if ( null != asResource )
             {
                 project = asResource.getProject();
             }
         }
         return project;
-    }
-
-    /**
-     * This method finds an adapter for an object to the given class using all the means available in eclipse.
-     * <ol>
-     * <li>If the object already implements the given class, return the same object.</li>
-     * <li>Else, check if the object is an IAdaptable and try its getAdapter() method</li>
-     * <li>Else, do a look up using the Platform Adapter Manager.</li>
-     * </ol>
-     * 
-     * @param clazz
-     *            class to adapt to
-     * @param object
-     *            the object to be adapted.
-     * @return
-     */
-    @SuppressWarnings( "unchecked" )
-    public static <T> T adaptAs( Class<T> clazz, Object object )
-    {
-        if ( object == null )
-        {
-            // can't adapt null
-            return null;
-        }
-        if ( clazz.isAssignableFrom( object.getClass() ) )
-        {
-            // the object is or extends the requested class
-            return (T) object;
-        }
-        if ( object instanceof IAdaptable )
-        {
-            // try to adapt through the interface
-            T adapted = (T) ( (IAdaptable) object ).getAdapter( clazz );
-            if ( adapted != null )
-            {
-                // adapting succeeded
-                return adapted;
-            }
-        }
-        // nothing worked, try the platform adapted manager
-        return (T) Platform.getAdapterManager().getAdapter( object, clazz );
     }
 
     public static IWorkbenchPage getActivePageInWorkbench()
